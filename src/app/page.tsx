c37d85c3a099a3d8af90b48ce6ff6dc8d5740b7a
Rafael <rflpeq.rs@gmail.com>
2024-03-20 07:34:10 -0300

    hotfix: add botton line at nav-bar and fix styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import logo from '../app/assets/images/logo.png';
 export default function Home() {
   return (
     <main>
-      <nav className="flex justify-between items-center ml-10 p-3 mr-40">
-        <Image src={logo} alt={"blog logo"} width={80}/>
-        <div className="justify-evenly">
+      <nav className="flex justify-between items-center border-b border-black w-full">
+        <Image src={logo} alt={"blog logo"} width={100} className="ml-10 p-3"/>
+        <div className="justify-evenly mr-48">
         <a href="#" className="mx-5">Início</a>
         <a href="#" className="mx-5">Posts</a>
         <a href="#" className="mx-5">Seja um motorista</a>
